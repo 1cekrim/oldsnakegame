672aec4ba75de88ecac513e04dfe45116eb4cd90
2019027192 <rla92233@hanyang.ac.kr>
2019-03-14 23:22:18 +0900

    잘못 올라간 폴더 삭제

--- env_snake.py
+++ env_snake.py
@@ -9,6 +9,9 @@ class Snake:
         self.height = height + 2
         self.width = width + 2
         self.board = np.zeros((3, self.height, self.width))
+        self.direction = 0
+        self.head = [height // 2, width // 2]
+
         
         for i in range(0, self.width):
             self.board[0][0][i] = 1
@@ -54,4 +57,25 @@ class Snake:
                 break
         self.put_food(y, x)
 
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1] 
 
+    def do_action(self, action):
+        reward = -0.1
+        is_ended = False
+        
+        if (action == 0):
+            #좌회전
+            self.direction -= 1
+        elif (action == 2):
+            #우회전
+            self.direction += 1
+            
+        if (direction < 0):
+            direction = 3
+        elif (direction > 3):
+            direction = 0
+
+        
+
+        return self.get_state(), action, reward, is_ended
